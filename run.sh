eabbe6985458450fdf25e6dcb4c4d13d30b07b42
BurhanYasir123 <burhanyasirburhan2@gmail.com>
2026-03-27 17:15:44 +0500

    fixed some things...

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
-glslc -fshader-stage=vert ./src/shaders/vertex.glsl -o ./src/vert.spv
-glslc -fshader-stage=frag ./src/shaders/fragment.glsl -o ./src/frag.spv
-g++ *.cpp -I./src -lvulkan -lglfw -o app
+mkdir ./build
+mkdir ./build/shaders
+glslc -fshader-stage=vert ./src/shaders/vertex.glsl -o ./build/shaders/vert.spv
+glslc -fshader-stage=frag ./src/shaders/fragment.glsl -o ./build/shaders/frag.spv
+g++ *.cpp -I./src -lvulkan -lglfw -o ./build/app
 echo --------------------------------
+pushd build
 ./app
+popd
